c05bcc27432396788c6d86ae3ea8fab55bad6cdf
Roflanpepe <95681667+NikDomin@users.noreply.github.com>
2022-12-11 13:57:23 +0200

    Camera and Moving

--- Epitome full course/Assets/Scripts/Player.cs
+++ Epitome full course/Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
     private Vector3 moveDelta;
+    private RaycastHit2D hit;
 
     private void Start()
     {
@@ -17,6 +18,7 @@ public class Player : MonoBehaviour
     {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
+
         //Reset MoveDelta;
         moveDelta = new Vector3(x,y,0);
 
@@ -27,6 +29,22 @@ public class Player : MonoBehaviour
         else if (moveDelta.x < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y),
+            Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+
+        if (hit.collider == null)
+        {
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0),
+            Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+
+        if (hit.collider == null)
+        {
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
         }
 
     }
